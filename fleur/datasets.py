4a85583c384e79d49d5f3cd9c5e509f421ca24e9
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-06-01 22:24:42 +0200

    document datasets

--- fleur/datasets.py
+++ fleur/datasets.py
@@ -2,71 +2,68 @@ import narwhals as nw
 from narwhals.typing import IntoDataFrame
 import os
 
+from typing import Dict, Optional
+
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
 AVAILABLE_DATASETS = ["iris", "mtcars"]
 AVAILABLE_OUTPUTS = ["pandas", "polars", "pyarrow", "modin", "cudf"]
 
 
-def _load_data(dataset_name: str, return_as: str, **kwargs) -> IntoDataFrame:
+def _load_data(dataset_name: str, backend: str, **kwargs) -> IntoDataFrame:
     """
-    Load one of the available datasets in fleur. This function is a minimalist wrapper around `narwhals.read_csv() <https://narwhals-dev.github.io/narwhals/api-reference/narwhals/#narwhals.read_csv>`_ function.
+    Load one of the available datasets in fleur. This function is a simple wrapper
+    around [`narwhals.read_csv()`](https://narwhals-dev.github.io/narwhals/api-reference/narwhals/#narwhals.read_csv)
+    function.
 
-    :param dataset_name: A string specifying the name of the dataset. Currently, only "iris" is supported.
-    :param return_as: The output format of the dataframe. Note that, for example, if you set ``return_as="polars"``, you must have polars installed. Must be one of the following: "pandas", "polars", "pyarrow", "modin", "cudf". Default to "pandas".
-    :param kwargs: Additional arguments passed to `narwhals.read_csv() <https://narwhals-dev.github.io/narwhals/api-reference/narwhals/#narwhals.read_csv>`_.
-    :return: A dataframe with the specified dataset.
+    Args:
+        dataset_name: A string specifying the name of the dataset. Currently, "iris" and "mtcars" are supported.
+        backend: The output format of the dataframe. Note that, for example, if you set `backend="polars"`, you must have polars installed. Must be one of the following: "pandas", "polars", "pyarrow", "modin", "cudf". Default to "pandas".
+        kwargs: Additional arguments passed to [`narwhals.read_csv()`](https://narwhals-dev.github.io/narwhals/api-reference/narwhals/#narwhals.read_csv).
+
+    Returns:
+        A dataframe with the specified dataset.
     """
     dataset_name = dataset_name.lower()
-    return_as = return_as.lower()
+    backend = backend.lower()
 
     if dataset_name not in AVAILABLE_DATASETS:
         raise ValueError(
             f"dataset_name must be one of: {' ,'.join(AVAILABLE_DATASETS)}"
         )
 
-    if return_as not in AVAILABLE_OUTPUTS:
-        raise ValueError(f"return_as must be one of: {' ,'.join(AVAILABLE_OUTPUTS)}")
+    if backend not in AVAILABLE_OUTPUTS:
+        raise ValueError(f"backend must be one of: {' ,'.join(AVAILABLE_OUTPUTS)}")
 
     dataset_file = f"{dataset_name}.csv"
     dataset_path = os.path.join(PACKAGE_DIR, "datasets", dataset_file)
-    df = nw.read_csv(dataset_path, backend=return_as, **kwargs).to_native()
+    df = nw.read_csv(dataset_path, backend=backend, **kwargs).to_native()
 
     return df
 
 
-def load_iris(return_as: str = "pandas", **kwargs) -> IntoDataFrame:
+def load_iris(backend: str = "pandas", **kwargs: Optional[Dict]) -> IntoDataFrame:
     """
     Load the iris dataset.
 
-    :param return_as: The output format of the dataframe. Note that, for example, if you set ``return_as="polars"``, you must have polars installed. Must be one of the following: "pandas", "polars", "pyarrow", "modin", "cudf". Default to "pandas".
-    :param kwargs: Additional arguments passed to `narwhals.read_csv() <https://narwhals-dev.github.io/narwhals/api-reference/narwhals/#narwhals.read_csv>`_.
-    :return: The iris dataset.
+    Args:
+        backend: The output format of the dataframe. Note that, for example, if you set `backend="polars"`, you must have polars installed. Must be one of the following: "pandas", "polars", "pyarrow", "modin", "cudf". Default to "pandas".
+        kwargs: Additional arguments passed to [`narwhals.read_csv()`](https://narwhals-dev.github.io/narwhals/api-reference/narwhals/#narwhals.read_csv).
 
-    Examples
-    --------
-
-    .. doctest::
-
-        >>> import fleur.datasets as datasets
-        >>> iris = datasets.load_iris()
+    Returns:
+        The iris dataset.
     """
-    return _load_data("iris", return_as=return_as, **kwargs)
+    return _load_data("iris", backend=backend, **kwargs)
 
 
-def load_mtcars(return_as: str = "pandas", **kwargs) -> IntoDataFrame:
+def load_mtcars(backend: str = "pandas", **kwargs: Optional[Dict]) -> IntoDataFrame:
     """
     Load the mtcars dataset.
 
-    :param return_as: The output format of the dataframe. Note that, for example, if you set ``return_as="polars"``, you must have polars installed. Must be one of the following: "pandas", "polars", "pyarrow", "modin", "cudf". Default to "pandas".
-    :param kwargs: Additional arguments passed to `narwhals.read_csv() <https://narwhals-dev.github.io/narwhals/api-reference/narwhals/#narwhals.read_csv>`_.
-    :return: The mtcars dataset.
+    Args:
+        backend: The output format of the dataframe. Note that, for example, if you set `backend="polars"`, you must have polars installed. Must be one of the following: "pandas", "polars", "pyarrow", "modin", "cudf". Default to "pandas".
+        kwargs: Additional arguments passed to [`narwhals.read_csv()`](https://narwhals-dev.github.io/narwhals/api-reference/narwhals/#narwhals.read_csv).
 
-    Examples
-    --------
-
-    .. doctest::
-
-        >>> import fleur.datasets as datasets
-        >>> mtcars = datasets.load_mtcars()
+    Returns:
+        The mtcars dataset.
     """
-    return _load_data("mtcars", return_as=return_as, **kwargs)
+    return _load_data("mtcars", backend=backend, **kwargs)
