b6a4b6815a2401e47301d48e38213d2a032d57bd
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-06-01 19:19:34 +0200

    fix input data handler

--- inferplot/data_input.py
+++ inferplot/data_input.py
@@ -1,9 +1,9 @@
 import narwhals as nw
-import numpy as np
+from narwhals.dependencies import is_numpy_array, is_into_series
 
 
 class InputDataHandler:
-    def __init__(self, x, y, data):
+    def __init__(self, x, y, data=None):
         # Case 1: x and y are strings referencing columns from a DataFrame
         if isinstance(x, str) and isinstance(y, str):
             if data is None:
@@ -20,11 +20,11 @@ class InputDataHandler:
             self.y_name = y
             self.source = "dataframe"
 
-        # Case 2: x and y are Series --> how to detect that?
-        elif isinstance(x, nw.Series) and isinstance(y, nw.Series):
+        # Case 2: x and y are Series-like
+        elif is_into_series(x) and is_into_series(y):
             self.data = None
-            self.x = x
-            self.y = y
+            self.x = nw.new_series("x", x, backend="pandas")
+            self.y = nw.new_series("y", y, backend="pandas")
             self.x_name = x.name or "x"
             self.y_name = y.name or "y"
             self.source = "series"
@@ -32,22 +32,20 @@ class InputDataHandler:
         # Case 3: x and y are array-like (list, numpy, etc.)
         elif self._is_array_like(x) and self._is_array_like(y):
             self.data = None
-            self.x = nw.new_series(x)
-            self.y = nw.new_series(y)
+            self.x = nw.new_series("x", x, backend="pandas")
+            self.y = nw.new_series("y", y, backend="pandas")
             self.x_name = "x"
             self.y_name = "y"
             self.source = "array"
 
         else:
             raise TypeError(
-                "`x` and `y` must be both strings (column names), both Series, or both array-like."
+                "`x` and `y` must be both strings (column names), both Series, or both array-like. "
+                f"Not type(x)={type(x)} and type(y)={type(y)}"
             )
 
-        if self.x.len() != self.y.len():
-            raise ValueError("`x` and `y` must have the same length.")
-
     def _is_array_like(self, obj):
-        return isinstance(obj, (list, tuple, np.ndarray))
+        return isinstance(obj, (list, tuple)) or is_numpy_array(obj)
 
     def get_info(self):
         return {
--- tests/test_input_data_handling.py
+++ tests/test_input_data_handling.py
@@ -1,4 +1,7 @@
 import narwhals as nw
+import pandas as pd
+import polars as pl
+import numpy as np
 
 from typing import Dict
 
@@ -19,33 +22,32 @@ def test_output_scheme(backend):
     info_series = InputDataHandler(x_series, y_series).get_info()
 
     expected_keys = [
+        "x",
+        "y",
         "x_name",
         "y_name",
         "source",
-        "x",
-        "y",
     ]
 
     for info in [info_df, info_series]:
         assert isinstance(info, Dict)
         assert list(info) == expected_keys
 
-    for key in expected_keys:
-        if key != "source":
-            assert info_df[key] == info_series[key]
-
     assert info_df["source"] == "dataframe"
     assert info_series["source"] == "series"
 
 
-@pytest.mark.parametrize("x", [[1, 2, 3, 4, 5], "height"])
-@pytest.mark.parametrize("y", [[1, 2, 3, 4, 5], "weight"])
-@pytest.mark.parametrize("backend", ["pandas", "polars"])
-def test_different_x_and_y_inputs(x, y, backend):
-    df = nw.from_dict(
-        {"height": [1, 2, 3, 4, 5], "weight": [1, 2, 3, 4, 5]}, backend=backend
-    )
-    info_df = InputDataHandler("height", "weight", data=df).get_info()
+@pytest.mark.parametrize(
+    ["x", "y"],
+    [
+        ([1, 2, 3], [1, 2, 3]),
+        (pd.Series([1, 2, 3]), pd.Series([1, 2, 3])),
+        (pl.Series([1, 2, 3]), pl.Series([1, 2, 3])),
+        (np.array([1, 2, 3]), np.array([1, 2, 3])),
+    ],
+)
+def test_different_x_and_y_inputs(x, y):
+    info_df = InputDataHandler(x, y).get_info()
 
-    assert info_df["x"] == nw.new_series(x, backend=backend)
-    assert info_df["y"] == nw.new_series(y, backend=backend)
+    assert info_df["x"] == nw.new_series("x", x, backend="pandas")
+    assert info_df["y"] == nw.new_series("y", y, backend="pandas")
